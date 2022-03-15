784e5d67521b409f891cff723fadcc336e87cf35
HuaManYing <3097634177@qq.com>
2022-03-15 22:37:10 +0800

    feat: husky,commitlint,lint-staged配置成功

--- .prettierrc.js
+++ .prettierrc.js
@@ -2,6 +2,6 @@ module.exports = {
   printWidth: 120, //单行长度
   tabWidth: 2, //缩进长度
   useTabs: false, //使用空格代替tab缩进
-  semi: false, //句末使用分号
+  semi: true, //句末使用分号
   singleQuote: true, //使用单引号
-};
+}
